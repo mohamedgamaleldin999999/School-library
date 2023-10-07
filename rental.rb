c2fee06672fffea4eed6d60105f32dfa02b26a28
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-10-07 20:44:13 +0300

    Edit rental

--- rental.rb
+++ rental.rb
@@ -1,13 +1,13 @@
 class Rental
-  def initialize(book, person)
-    @date = DateTime.now
+  attr_accessor :date
+  attr_reader :person, :book
+
+  def initialize(book:, person:, date:)
+    @date = date || Date.now
     @book = book
     @person = person
 
     book.rentals << self
     person.rentals << self
   end
-
-  attr_accessor :date
-  attr_reader :person, :book
 end
