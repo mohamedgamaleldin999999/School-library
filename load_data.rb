0edf24143b07aba4c93573efa8d473a6a3276755
mohamedgamaleldin999999 <123296170+mohamedgamaleldin999999@users.noreply.github.com>
2023-10-08 01:30:44 +0300

    Fix linters

--- load_data.rb
+++ load_data.rb
@@ -1,7 +1,6 @@
 require 'date'
 class LoadData
   def self.load_data(books, people, classrooms, rentals)
-    ## populate @books
     books_dummy_data = [
       ['The Grass is Always Greener', 'Jeffrey Archer'], ['Murder!', 'Arnold Bennett'],
       ['A Boy at Seven', 'John Bidwell'], ['The Open Boat', 'Stephen Crane'], ['The Higgler', 'A. E. Coppard']
@@ -9,8 +8,7 @@ class LoadData
     books_dummy_data.each do |book|
       books.push(Book.new(book[0], book[1]))
     end
-    students_names = %w[John Ana Kevin]
-    students_names.each do |name|
+    students_names = %w[John Ana Kevin] students_names.each do |name|
       people.push(Student.new(classroom: 'Alpha', age: 22, name: name, parent_permission: true))
     end
     teachers_names = %w[John Ana Kevin]
